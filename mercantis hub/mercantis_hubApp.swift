ddfc4430e9d44a959203b0d3b21fd991b150e3a9
Kevin Busuttil <kevin@busuttil-technologies.com>
2026-04-12 23:18:14 +0200

    Initial Commit

---
+++ mercantis hub/mercantis_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercantis_hubApp.swift
+//  mercantis hub
+//
+//  Created by Kevin Busuttil on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct mercantis_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
